924e90c40cee5714c094e3b5e6b95dbd285c8e36
AmmarJ23 <ammar.jmldn@outlook.com>
2022-03-13 14:03:23 +0800

    listen to voice then store all into string

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import pyaudio
 import PySimpleGUI as sg
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+mainText = " "
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+while(True):
+
+    #listen for speech
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Recording...")
+        audio = r.listen(source)
+
+    #transcribe speech into string
+    try:
+        transcribedText = r.recognize_google(audio)
+        print("Google Speech Recognition successful: " + transcribedText)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    #add transcribed text into main text file
+    mainTextTemp = str(transcribedText)
+    mainText = mainText + " " + mainTextTemp
+
+    with open("Transcribed Text.txt", 'w') as f:
+        f.write(mainText)
+        f.close
