852a41dd23a22070a861dd9ec0b7ca3d0cd1493b
AmmarJ23 <ammar.jmldn@outlook.com>
2022-03-20 22:54:13 +0800

    Create main (old).py

---
+++ main (old).py
@@ -0,0 +1,63 @@
+import pyaudio
+import speech_recognition as sr
+
+mainTextEN = mainTextCH = mainTextMY = ""
+
+while(True):
+
+    #listen for speech
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Recording...")
+        audio = r.listen(source)
+
+    #recognise english
+    try:
+        transcribedTextEN = r.recognize_google(audio, language="en-GB")
+        print("Google Speech Recognition - English: " + transcribedTextEN)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    #recognise chinese
+    try:
+        transcribedTextCH = r.recognize_google(audio, language="zh")
+        print("Google Speech Recognition - Chinese: " + transcribedTextCH)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    #recognise malay
+    try:
+        transcribedTextMY = r.recognize_google(audio, language="ms-My")
+        print("Google Speech Recognition - Malay: " + transcribedTextMY)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    #english transcribed text into main file
+    mainTextTempEN = str(transcribedTextEN)
+    mainTextEN = mainTextEN + ". " + mainTextTempEN
+
+    #chinese transcribed text into main file
+    mainTextTempCH = str(transcribedTextCH)
+    mainTextCH = mainTextCH + ". " + mainTextTempCH
+
+    #malay transcribed text into main file
+    mainTextTempMY = str(transcribedTextMY)
+    mainTextMY = mainTextMY + ". " + mainTextTempMY
+
+    with open("Transcribed_Text\Transcribed Text - EN.txt", 'w') as f:
+        f.write(mainTextEN)
+        f.close
+    
+    #with open("Transcribed_Text\Transcribed Text - CH.txt", 'w') as f:
+    #    f.write(mainTextCH)
+    #    f.close
+
+    with open("Transcribed_Text\Transcribed Text - MY.txt", 'w') as f:
+        f.write(mainTextMY)
+        f.close
