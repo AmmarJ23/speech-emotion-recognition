297583931b5b4c9718486c7f6e3d9c2df3c615ad
AmmarJ23 <ammar.jmldn@outlook.com>
2022-03-20 22:56:09 +0800

    Implemented Emotion Recognition & Refactored Code
    -Implement emotions.py to recognise the emotion from spoken speech
    -Users can choose which language to speak in
    -Refactored code

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 import pyaudio
 import speech_recognition as sr
+from emotions import create_feature, vectorizer, clf, emoji_dict
 
-mainTextEN = mainTextCH = mainTextMY = ""
+def processAudio(userChoice, audio):
+
+    #user chooses english
+    if(userChoice == 0):
+        #recognise english
+        try:
+            transcribedTextEN = r.recognize_google(audio, language="en-GB")
+            print("Google Speech Recognition - English: " + transcribedTextEN)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print(transcribedTextEN)
+        return transcribedTextEN
+        
+    #user chooses chinese
+    if(userChoice == 1):
+         #recognise chinese
+        try:
+            transcribedTextCH = r.recognize_google(audio, language="zh")
+            print("Google Speech Recognition - Chinese: " + transcribedTextCH)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return transcribedTextCH
+
+    #user chooses malay
+    if(userChoice == 2):
+        #recognise malay
+        try:
+            transcribedTextMY = r.recognize_google(audio, language="ms-My")
+            print("Google Speech Recognition - Malay: " + transcribedTextMY)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return transcribedTextMY
+
+mainTextEN = mainTextCH = mainTextMY = mainText = ""
+userChoice = 0
 
 while(True):
+
+    print("[0]=English\n[1]=Chinese\n[2]=Malay")
+    input(userChoice)
 
     #listen for speech
     r = sr.Recognizer()
@@ -11,53 +55,15 @@ while(True):
         print("Recording...")
         audio = r.listen(source)
 
-    #recognise english
-    try:
-        transcribedTextEN = r.recognize_google(audio, language="en-GB")
-        print("Google Speech Recognition - English: " + transcribedTextEN)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    #recognise chinese
-    try:
-        transcribedTextCH = r.recognize_google(audio, language="zh")
-        print("Google Speech Recognition - Chinese: " + transcribedTextCH)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    #recognise malay
-    try:
-        transcribedTextMY = r.recognize_google(audio, language="ms-My")
-        print("Google Speech Recognition - Malay: " + transcribedTextMY)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    #english transcribed text into main file
-    mainTextTempEN = str(transcribedTextEN)
-    mainTextEN = mainTextEN + ". " + mainTextTempEN
-
-    #chinese transcribed text into main file
-    mainTextTempCH = str(transcribedTextCH)
-    mainTextCH = mainTextCH + ". " + mainTextTempCH
-
-    #malay transcribed text into main file
-    mainTextTempMY = str(transcribedTextMY)
-    mainTextMY = mainTextMY + ". " + mainTextTempMY
-
-    with open("Transcribed_Text\Transcribed Text - EN.txt", 'w') as f:
-        f.write(mainTextEN)
+    mainTextTemp = processAudio(userChoice, audio)
+    mainText = mainText + "." + mainTextTemp
+    
+    with open("Transcribed_Text\Transcribed Text.txt", 'w') as f:
+        f.write(mainText)
         f.close
     
-    #with open("Transcribed_Text\Transcribed Text - CH.txt", 'w') as f:
-    #    f.write(mainTextCH)
-    #    f.close
-
-    with open("Transcribed_Text\Transcribed Text - MY.txt", 'w') as f:
-        f.write(mainTextMY)
-        f.close
+    features = create_feature(mainText)
+    features = vectorizer.transform(features)
+    prediction = clf.predict(features)[0]
+    print(mainText, emoji_dict[prediction])
+    print("\n")
