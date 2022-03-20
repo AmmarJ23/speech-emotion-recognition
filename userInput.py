2f1f8f89e0e121d162ea3649264702f1f0fcaac6
AmmarJ23 <ammar.jmldn@outlook.com>
2022-03-20 15:44:06 +0800

    Added user input for emotion detection
    https://thecleverprogrammer.com/2021/02/19/text-emotions-detection-with-machine-learning/

---
+++ userInput.py
@@ -0,0 +1,27 @@
+from emotions import create_feature, vectorizer, clf, emoji_dict
+
+"""
+t1 = "I don't know what to do after my father passed away yesterday"
+t2 = "I will hit him if he does not shut up"
+t3 = "My dog died yesterday"
+t4 = "I don't love you anymore..!"
+
+texts = [t1, t2, t3, t4]
+for text in texts: 
+    features = create_feature(text, nrange=(1, 4))
+    features = vectorizer.transform(features)
+    prediction = clf.predict(features)[0]
+    print( text,emoji_dict[prediction])
+"""
+
+while (True):
+    textInput = input("")
+
+    if(textInput == "0"):
+        break
+
+    features = create_feature(textInput)
+    features = vectorizer.transform(features)
+    prediction = clf.predict(features)[0]
+    print(textInput, emoji_dict[prediction])
+    print("\n")
